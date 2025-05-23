abccdb0c24ae13709cebd82d23563bc454f1db84
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-05-23 11:57:39 +0200

    added loading-overlay

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -62,6 +62,10 @@ form.addEventListener('submit', function(event){
     console.log('submit prevented!!');
     card.update({'disabled':true});
     $('#submit-button').attr('disabled',true);
+    //$('#payment-form').fadeToggle(100);
+    $('#payment-form').fadeToggle(100);
+    //$('#loading-overlay').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -69,16 +73,17 @@ form.addEventListener('submit', function(event){
     }).then(function(result) {
         if (result.error) {
              var errorDiv = document.getElementById('card-errors');
-             var html =
-            `<span class="icon" role = "alert">
+             var html = `
+                <span class="icon" role = "alert">
                 <i class="fas fa-times"></i>
-            </span>
-            <span>${result.error.message}</span>
-            `
-        $(errorDiv).html(html)
-        card.update({'disabled':false});
-        $('#submit-button').attr('disabled',false);
-        console.log('Not done!!')
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
+            card.update({'disabled':false});
+            $('#submit-button').attr('disabled',false);
+            console.log('Not done!!');
         } else {
             if (result.paymentIntent.status==='succeeded') {
                 console.log('Succedded!!')
