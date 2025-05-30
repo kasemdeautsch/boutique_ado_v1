e17fc9ede601d27fe55f6907e4619bc95a417d3b
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-05-30 13:07:42 +0200

    Product Admin Product Form

--- checkout/forms.py
+++ checkout/forms.py
@@ -27,7 +27,8 @@ class OrderForm(forms.ModelForm):
             'street_address2': 'Street Address 2',
             'county': 'County State or Locality',
         }
-        #print(self.fields)
+        print('self.fields: ', self.fields)
+        print('self.fields.items: ', self.fields.items())
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if field !='country':
