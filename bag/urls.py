7260b10aeded907212aa91876a2a731b14de9f96
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-06-02 11:23:30 +0200

    to deploy2

--- bag/urls.py
+++ bag/urls.py
@@ -1,7 +1,7 @@
 
 #from django.contrib import admin
 from django.urls import path
-from . import views
+from bag import views
 
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
--- bag/views.py
+++ bag/views.py
@@ -11,7 +11,7 @@ def view_bag(request):
     A view that renders the bag content page
     """
     print("about to render...")
-    
+
     return render(
         request,
         "bag/bag.html"
@@ -47,7 +47,7 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
-                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]['items_by_size'][size]}')
+                messages.success(request, f"Updated size {size.upper()} {product.name} quantity to {bag[item_id]['items_by_size'][size]}")
                # print('------------------------')
                 #print('bag1: ', bag)
                 #print("bag[item_id]['items_by_size'].keys()1: ", bag[item_id]['items_by_size'].keys())
@@ -104,7 +104,7 @@ def adjust_bag(request, item_id):
     if size:
         if quantity > 0:
             bag[item_id]['items_by_size'][size] = quantity
-            messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]['items_by_size'][size]}')
+            messages.success(request, f"Updated size {size.upper()} {product.name} quantity to {bag[item_id]['items_by_size'][size]}")
         else:
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
