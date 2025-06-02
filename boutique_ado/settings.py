1d7fbb4f8be8af4c6693e337063901e39da3283b
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-06-02 10:54:13 +0200

    to deploy

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 if os.path.isfile('env.py'):
     import env
@@ -25,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2m*+-xqpo7_-w!f5bg&^d5)a@zpwwh8wldx5k195g(e#uh4x-*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,6 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1', #  VS Code preview
     'localhost', # listen for stripe webhooks
+    'boutique-ado-store-v1-14583a591052.herokuapp.com',
 
     ]
 
@@ -129,13 +131,22 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+    
 
+# DATABASES = {
+#    'default': dj_database_url.parse('postgresql://neondb_owner:s5p7NKmIbodG@ep-plain-waterfall-a2738bs3.eu-central-1.aws.neon.tech/baker_boxer_slash_976327')
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
